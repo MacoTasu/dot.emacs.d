4db7cafd6fbb8ffd1fdde2ee64b8580ff307d378
MacoTasu <makomacomako@me.com>
2013-11-27 10:49:27 +0900

    add flymake

--- init.el
+++ init.el
@@ -10,3 +10,14 @@
 (setq inits_dir (expand-file-name "~/.emacs.d/inits/"))
 (init-loader-load inits_dir)
 
+(require 'package)
+
+; Add package-archives
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/")) ; ついでにmarmaladeも追加
+
+; Initialize
+(package-initialize)
+
+; melpa.el
+(require 'melpa)

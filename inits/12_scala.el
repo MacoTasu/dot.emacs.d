8333c11b379c2cbd6b28cef1d178e7acb7b7c8f0
MacoTasu <makomacomako@me.com>
2014-11-05 20:07:09 +0900

    scala-modeを追加

--- inits/12_scala.el
+++ inits/12_scala.el
@@ -1,13 +1,5 @@
 ;; ~/.emacs.d/init.el
  
-(require 'scala-mode)
+(add-to-list 'load-path "~/.emacs.d/scala-mode")
+(require 'scala-mode-auto)
 (add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
-(add-hook 'scala-mode-hook
-  (function
-    (lambda ()
-      (setq scala-mode-indent:step 4)
-      (scala-mode-lib:define-keys scala-mode-map
-                                  ([(shift tab)]   'scala-undent-line)
-                                  ([(control tab)] nil))
-      (local-set-key [(return)] 'newline-and-indent))))
-(add-hook 'scala-mode-hook 'jaspace-mode)
